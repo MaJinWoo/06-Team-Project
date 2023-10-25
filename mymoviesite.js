bb3d1335d1b36ca42f7799af1603d6164cf93d3c
손지원 <sonjiwon@sonjiwon-ui-MacBookAir.local>
2023-10-26 02:09:09 +0900

    저장

--- mymoviesite.js
+++ mymoviesite.js
@@ -28,8 +28,12 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1', opti
 
 
 // 섹션 1 미리보기 카드 
- 
+// 1. 카드를 가져온다
+document.querySelector(".card_detail").innerHTML =
 
+
+// 2. 실제 데이터를 넣어준다
+// 3. 랜덤으로 데이터를 돌린다. 
 
 
 
